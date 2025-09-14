aba8b9d4cc6e6870702e7ce01f5f0fdd411b4cf4
NuVega <47011763+NuVega@users.noreply.github.com>
2025-09-14 14:04:14 +0300

    Initial Commit

---
+++ CamPiP/CamPiPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamPiPApp.swift
+//  CamPiP
+//
+//  Created by rippie on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CamPiPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamPiPTests/CamPiPTests.swift
@@ -0,0 +1,17 @@
+//
+//  CamPiPTests.swift
+//  CamPiPTests
+//
+//  Created by rippie on 14.09.2025.
+//
+
+import Testing
+@testable import CamPiP
+
+struct CamPiPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
